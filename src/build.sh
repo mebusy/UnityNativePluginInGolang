e0ba3a5f4f4e0eb2a704ae4387b27b3f872a4b7f
mebusy <qibinyi@mebusys-imac.ubisoft.org>
2020-09-22 12:26:25 +0800

    +build.sh

---
+++ src/build.sh
@@ -0,0 +1,10 @@
+# to list all GOOS/GOARCH:  go tool dist list
+
+# c-shared
+
+
+# OSX
+DIST="../dist/OSX/"
+mkdir -p $DIST
+GOOS=darwin GOARCH=amd64 go build -buildmode=c-archive -o $DIST/libgoOSX.a libgo.go
+
