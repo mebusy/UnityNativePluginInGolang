3cc1c0f9717eacbe4d1748f04496f4048bd9b678
mebusy <qibinyi@mebusys-imac.ubisoft.org>
2020-09-22 18:24:27 +0800

    m

--- src/helper.h
+++ src/helper.h
@@ -1,7 +1,7 @@
 #ifndef CGO_LIB_HELPER_H
 #define CGO_LIB_HELPER_H
 
-typedef char* (*Callback_Str_Str)(const char* arg);
-extern const char* bridge_func_callback_str_str(Callback_Str_Str f, char* s);
+typedef char* (*Callback_S_S)(const char* arg);
+extern const char* bridge_func_callback_str_str(Callback_S_S f, char* s);
 
 #endif
