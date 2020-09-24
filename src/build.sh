cbecf3fe338b69c0ed2b4f34c19d7ac695ea6b62
mebusy <qibinyi@mebusys-imac.ubisoft.org>
2020-09-24 17:27:27 +0800

    mm

--- src/build.sh
+++ src/build.sh
@@ -1,23 +1,61 @@
 # to list all GOOS/GOARCH:  go tool dist list
+PLATFORM=$1
 
-# c-shared
+if [ "$PLATFORM" = "" ] ; then
+    echo "must specify a platform"
+    exit 1
+fi
+
 
 # OSX
 DIST="../dist/OSX/"
 mkdir -p $DIST
-echo $DIST
+# always build Darwin ...
 GOOS=darwin GOARCH=amd64 go build -v -ldflags "-w" -buildmode=c-archive -o $DIST/libgo.a  libgo/
-# build OSX bundle
-xcodebuild -project ../OSxBundle/OSXBundle.xcodeproj -scheme OSXBundle  -configuration Release
 
-# Win64
-cp -f $DIST/libgo.h .
-DIST="../dist/Win64/"
-echo $DIST
-mkdir -p $DIST
-cp -f ../Plugin.cpp .
-GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc  CXX=x86_64-w64-mingw32-g++  go build -v -ldflags "-w" -buildmode=c-shared -o $DIST/libgo.dll  libgo/
-rm -f ./Plugin.cpp
-rm -f ./libgo.h
+if [ "$PLATFORM" = "Darwin" ] ; then
+    echo Building Plugin for $PLATORM
 
-mv $DIST/libgo.dll ../Unity_PluginTest/Assets/Plugins/
+    # build
+
+    echo $DIST
+    # build OSX bundle
+    xcodebuild -project ../OSxBundle/OSXBundle.xcodeproj -scheme OSXBundle  -configuration Release
+fi
+
+if [ "$PLATFORM" = "Windows" ] ; then
+    echo Building Plugin for $PLATORM
+    
+    # Win64
+    cp -f $DIST/libgo.h .
+    DIST="../dist/Win64/"
+    echo $DIST
+    mkdir -p $DIST
+    cp -f ../Plugin.cpp .
+    GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc  CXX=x86_64-w64-mingw32-g++  go build -v -ldflags "-w" -buildmode=c-shared -o $DIST/libgo.dll  libgo/
+    rm -f ./Plugin.cpp
+    rm -f ./libgo.h
+
+    mv $DIST/libgo.dll ../Unity_PluginTest/Assets/Plugins/
+fi
+
+if [ "$PLATFORM" = "Android" ] ; then
+    echo Building Plugin for $PLATORM
+
+    cp -f $DIST/libgo.h .
+
+    # Android
+    ARM_GCC="$NDK_ROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64/bin/arm-linux-androideabi-gcc"
+
+    DIST="../dist/Win64/"
+    echo $DIST
+    mkdir -p $DIST
+    cp -f ../Plugin.cpp .
+    GOOS=android GOARCH=arm CGO_ENABLED=1 CC=$ARM_GCC  go build -v -ldflags "-w" -buildmode=c-shared -o $DIST/libgo.so  libgo/
+    rm -f ./Plugin.cpp
+    rm -f ./libgo.h
+fi
+
+
+
+
