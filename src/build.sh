c65257dd596b850957ed062ebe2bd97cd1be1a35
mebusy <qibinyi@mebusys-imac.ubisoft.org>
2020-09-24 14:42:50 +0800

    m

--- src/build.sh
+++ src/build.sh
@@ -7,6 +7,8 @@ DIST="../dist/OSX/"
 mkdir -p $DIST
 echo $DIST
 GOOS=darwin GOARCH=amd64 go build -v -ldflags "-w" -buildmode=c-archive -o $DIST/libgo.a  libgo/
+# build OSX bundle
+xcodebuild -project ../OSxBundle/OSXBundle.xcodeproj -scheme OSXBundle  -configuration Release
 
 # Win64
 cp -f $DIST/libgo.h .
@@ -14,7 +16,7 @@ DIST="../dist/Win64/"
 echo $DIST
 mkdir -p $DIST
 cp -f ../Plugin.cpp .
-GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc  CXX=x86_64-w64-mingw32-g++  go build -v -x -ldflags "-w" -buildmode=c-shared -o $DIST/libgo.dll  libgo/
+GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc  CXX=x86_64-w64-mingw32-g++  go build -v -ldflags "-w" -buildmode=c-shared -o $DIST/libgo.dll  libgo/
 rm -f ./Plugin.cpp
 rm -f ./libgo.h
 
