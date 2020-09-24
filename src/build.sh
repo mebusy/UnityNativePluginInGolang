124abaeeb912bcc5a0494f6cc8a300ac89c0f97a
mebusy <qibinyi@mebusys-imac.ubisoft.org>
2020-09-24 19:07:04 +0800

    m

--- src/build.sh
+++ src/build.sh
@@ -8,16 +8,14 @@ if [ "$PLATFORM" = "" ] ; then
 fi
 
 
-# OSX
-DIST="../dist/OSX/"
-mkdir -p $DIST
-# always build Darwin ...
-GOOS=darwin GOARCH=amd64 go build -v -ldflags "-w" -buildmode=c-archive -o $DIST/libgo.a  libgo/
 
 if [ "$PLATFORM" = "Darwin" ] ; then
     echo Building Plugin for $PLATORM
 
-    # build
+    # OSX
+    DIST="../dist/OSX/"
+    mkdir -p $DIST
+    GOOS=darwin GOARCH=amd64 go build -v -ldflags "-w" -buildmode=c-archive -o $DIST/libgo.a  libgo/
 
     echo $DIST
     # build OSX bundle
@@ -28,22 +26,18 @@ if [ "$PLATFORM" = "Windows" ] ; then
     echo Building Plugin for $PLATORM
     
     # Win64
-    cp -f $DIST/libgo.h .
     DIST="../dist/Win64/"
     echo $DIST
     mkdir -p $DIST
     cp -f ../Plugin.cpp .
     GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc  CXX=x86_64-w64-mingw32-g++  go build -v -ldflags "-w" -buildmode=c-shared -o $DIST/libgo.dll  libgo/
     rm -f ./Plugin.cpp
-    rm -f ./libgo.h
 
     mv $DIST/libgo.dll ../Unity_PluginTest/Assets/Plugins/
 fi
 
 if [ "$PLATFORM" = "Android" ] ; then
     echo Building Plugin for $PLATORM
-
-    cp -f $DIST/libgo.h .
 
     # Android
     TOOLCHAIN="$NDK/toolchains/llvm/prebuilt/darwin-x86_64"
