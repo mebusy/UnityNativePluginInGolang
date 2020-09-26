5780327caa941607b1682808fec8bdf1812d72d3
Qi Binyi <qibinyi@Qis-mini.local>
2020-09-26 11:05:10 +0800

    m

--- src/libgo.go
+++ src/libgo.go
@@ -1,7 +1,7 @@
 package main
 
 // #cgo windows CPPFLAGS: -D_PLATFORM_WINDOWS=1
-// #cgo android CPPFLAGS: -march=armv7-a  -mfpu=neon -mfloat-abi=hard
+// #cgo android CPPFLAGS: -march=armv7-a -mfpu=neon -mfloat-abi=hard
 // #cgo android LDFLAGS:  -Wl,--no-warn-mismatch
 // #include "helper.h"
 // #include <stdlib.h>
