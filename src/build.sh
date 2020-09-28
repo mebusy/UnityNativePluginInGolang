6cbf35c875fbf25bc6606a71a6dd7dbd4dd16b27
mebusy <qibinyi@mebusys-imac.ubisoft.org>
2020-09-28 11:11:00 +0800

    update windows

--- src/build.sh
+++ src/build.sh
@@ -29,9 +29,7 @@ if [ "$PLATFORM" = "Windows" ] ; then
     DIST="../dist/Win64/"
     echo $DIST
     mkdir -p $DIST
-    cp -f ../Plugin.cpp .
     GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc  CXX=x86_64-w64-mingw32-g++  go build -v -ldflags "-w" -buildmode=c-shared -o $DIST/libgo.dll  libgo/
-    rm -f ./Plugin.cpp
 
     cp -f $DIST/libgo.dll ../Unity_PluginTest/Assets/Plugins/
 fi
